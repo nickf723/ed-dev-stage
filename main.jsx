c70aedf121ed814d1113bb7b0e03ca04024d181d
Nick <nickfarley723@gmail.com>
2025-11-13 22:18:38 -0500

    oops

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/globals.css";
+import { Layout } from "./app/layout.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>
+);
